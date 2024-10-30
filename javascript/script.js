5093bd13c2abe4c6549ee01cba2f113f4aaa2bfc
arthurrodrigues1 <167815228+arthurrodrigues1@users.noreply.github.com>
2024-10-30 18:46:36 -0300

    a

--- javascript/script.js
+++ javascript/script.js
@@ -6,16 +6,25 @@ let c = 10
 
 let d = 2
 
-let k = ( b < a * 2) && ( d < c - b)
-    k = (8 < 5 * 2) && (2 < 10 - 8)
+let k = (8 < 5 * 2) && (2 < 10 - 8)
     k = (8 < 10) && ( 2 < 2)
     k = true && false
     k = false
 
 let x = (a > b) || ! (c % 2 == 0)
+    x = (5 > 8) || ! (10 % 2 == 0)
+    x = false or not true
+    x = false
 
-let y = x || ! ( c > a + b / d)
+let y = x || ! ( c < a + b / d)
+    y = false or not (10 < 9)
+    y = false or not false
+    y = true
 
 let z = ! x && false || (d + a <= b + d)
+    z = not false and false or (2 + 5 <= 8 + 2)
+    z = not false and false ou true
+    z = true and true 
+    z = true
 
 console.log(k,x,y,z)
