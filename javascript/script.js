1ad9b58dce331e696f7ea3d97997f6963b0a77c8
arthurrodrigues1 <167815228+arthurrodrigues1@users.noreply.github.com>
2024-10-30 20:40:24 -0300

    a

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,14 @@
-let num1 = parseInt(prompt("digite um numero"))
+    let num1 = parseInt(prompt("digite o primeiro numero"))
+    let num2 = parseInt(prompt("digite o segundo numero"))
 
-let num2 = parseInt(prompt("digite um numero"))
-
-a = num1 + num2
-
-b = num1 / num2
-
-c = num1 * num2
-
-d = num1 - num2
-
-console.log(a,b,c,d)
+    if (num1 > num2){
+        console.log( "o primeiro numero e maior")
+    }else  if (num2 > num1){
+        console.log ("o segundo numero e maior")
+    }  else if (num1 == num2){
+        console.log( "os numeros sao iguais")
+    } else if (num2 == num1){
+        console.log( "os numeros sao iguais")
+    } else {
+        console.log (erro)
+    }
