c09f17091265132ad9e8cf67b15c7c85ddf4c0bf
arthurrodrigues1 <167815228+arthurrodrigues1@users.noreply.github.com>
2024-10-30 19:18:36 -0300

    a

--- javascript/script.js
+++ javascript/script.js
@@ -1,30 +1,9 @@
-let a = 5
+let nome =  prompt("digite seu nome")
 
-let b = 8
+let sobrenome = prompt("digite seu sobrenome")
 
-let c = 10
+let idade = parseInt(prompt("digite sua idade"))
 
-let d = 2
+let sexo = prompt("digite seu sexo")
 
-let k = (8 < 5 * 2) && (2 < 10 - 8)
-    k = (8 < 10) && ( 2 < 2)
-    k = true && false
-    k = false
-
-let x = (a > b) || ! (c % 2 == 0)
-    x = (5 > 8) || ! (10 % 2 == 0)
-    x = false or not true
-    x = false
-
-let y = x || ! ( c < a + b / d)
-    y = false or not (10 < 9)
-    y = false or not false
-    y = true
-
-let z = ! x && false || (d + a <= b + d)
-    z = not false and false or (2 + 5 <= 8 + 2)
-    z = not false and false ou true
-    z = true and true 
-    z = true
-
-console.log(k,x,y,z)
+console.log( nome, sobrenome, idade, sexo)
