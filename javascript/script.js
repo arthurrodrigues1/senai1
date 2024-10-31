6648bf8e70d4c248782c51d1afab683c69178727
arthurrodrigues1 <167815228+arthurrodrigues1@users.noreply.github.com>
2024-10-30 21:40:47 -0300

    a

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,33 @@
-    let num1 = parseInt(prompt("digite o primeiro numero"))
-    let num2 = parseInt(prompt("digite o segundo numero"))
+let valor = parseInt(prompt("digite um valor em metros"))
 
-    if (num1 > num2){
-        console.log( "o primeiro numero e maior")
-    }else  if (num2 > num1){
-        console.log ("o segundo numero e maior")
-    }  else if (num1 == num2){
-        console.log( "os numeros sao iguais")
-    } else if (num2 == num1){
-        console.log( "os numeros sao iguais")
-    } else {
-        console.log (erro)
-    }
+let mm = valor * 1000
+let cm = valor * 100
+let dm = valor * 10
+let dam = valor / 10
+let hm = valor / 100
+let km = valor / 1000
+
+let unidade = prompt("digite qual unidade voce quer mm,cm,dm,dam,hm,km")
+
+switch (unidade) {
+    case "mm":
+    console.log(mm)
+    break
+    case "cm":
+    console.log(cm)
+    break
+    case "dm":
+    console.log(dm)
+    break
+    case "dam":
+    console.log(dam)
+    break
+    case "hm":
+    console.log(hm)
+    break
+    case "km":
+    console.log(km)
+    break
+    default:
+    console.log("valor invalido")
+}
