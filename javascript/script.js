f8f0f503c233b0f4b293777370b4db28532b0e64
arthurrodrigues1 <167815228+arthurrodrigues1@users.noreply.github.com>
2024-11-01 19:08:54 -0300

    a

--- javascript/script.js
+++ javascript/script.js
@@ -1,33 +1,9 @@
-let valor = parseInt(prompt("digite um valor em metros"))
+let num = parseInt(prompt("digite um numero entre 0 e 10"))
 
-let mm = valor * 1000
-let cm = valor * 100
-let dm = valor * 10
-let dam = valor / 10
-let hm = valor / 100
-let km = valor / 1000
-
-let unidade = prompt("digite qual unidade voce quer mm,cm,dm,dam,hm,km")
-
-switch (unidade) {
-    case "mm":
-    console.log(mm)
-    break
-    case "cm":
-    console.log(cm)
-    break
-    case "dm":
-    console.log(dm)
-    break
-    case "dam":
-    console.log(dam)
-    break
-    case "hm":
-    console.log(hm)
-    break
-    case "km":
-    console.log(km)
-    break
-    default:
-    console.log("valor invalido")
+if (num > 10){
+    alert("numero invalido")
+}else{
+    for (let i = num; i < 11; i++) {
+    console.log(i);
+}
 }
